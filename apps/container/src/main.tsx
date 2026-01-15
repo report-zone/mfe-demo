0256518333f43475d85a6f3375d3bd13e655ed32
Olaf Becker <report.zone.dev@gmail.com>
2026-01-15 09:32:42 -0800

    remove private info

--- apps/container/src/main.tsx
+++ apps/container/src/main.tsx
@@ -6,10 +6,9 @@ import App from './App';
 const awsConfig = {
   Auth: {
     Cognito: {
-      userPoolId: import.meta.env.VITE_COGNITO_USER_POOL_ID || 'us-east-1_wQ130IObD',
-      userPoolClientId: import.meta.env.VITE_COGNITO_CLIENT_ID || '73isvistqelpjmksjvshopird9',
-      //userPoolClientId: import.meta.env.VITE_CLIENT_ID || '6a8duirf74r73pada6o6p45l37',
-      region: import.meta.env.VITE_AWS_REGION || 'us-east-1',
+      userPoolId: import.meta.env.VITE_COGNITO_USER_POOL_ID || 'your-pool-id',
+      userPoolClientId: import.meta.env.VITE_COGNITO_CLIENT_ID || 'your-client-id',
+      region: import.meta.env.VITE_AWS_REGION || 'your-region',
       authFlowType: 'USER_PASSWORD_AUTH',
     },
   },
